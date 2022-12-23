d3405077ae62d28f3d07e3017e8f5c37fccc0235
include-daodung <79931452+include-daodung@users.noreply.github.com>
2022-12-23 10:54:17 +0700

    commti dung

---
+++ DomainLayer/Models/SanPham.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class SanPham
+    {
+        [Key]
+        public int Id { get; set; }
+        public string RollNo { get; set; }
+        public int DanhMucSanPhamId { get; set; }
+        public string TenSanPham { get; set; }
+        public string ThongTin { get; set; }
+        public string Image { get; set; }
+        public decimal GiaTien { get; set; }
+        public DateTime NgayTao { get; set; }
+        public DateTime NgaySua { get; set; }
+        public int GiamGiaId { get; set; }
+    }
+}
---
+++ DomainLayer/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public int RoleId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public DateTime NgayTao { get; set; }
+        public DateTime NgaySua { get; set; }
+        public string Avatar { get; set; }
+
+    }
+}
