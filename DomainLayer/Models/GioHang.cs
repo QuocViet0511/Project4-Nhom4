db9a4996bbe01ad4d81f702c81b2589ddff71c87
include-daodung <79931452+include-daodung@users.noreply.github.com>
2022-12-27 22:16:40 +0700

    cmomi

--- DomainLayer/Models/GioHang.cs
+++ DomainLayer/Models/GioHang.cs
@@ -4,7 +4,7 @@ namespace DomainLayer.Models
 {
     public class GioHang : BaseEntity
     {
-        public string SanPhamId { get; set; }
+        public int SanPhamId { get; set; }
         public int UserId { get; set; }
         public int SoLuong { get; set; }
         public decimal TongTien { get; set; }
